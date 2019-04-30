ce6e3dfb86179889e51079397e3746b20c8edf05
Steven Hatzakis <hatgit@users.noreply.github.com>
2019-04-30 18:56:06 -0400

    Updated version + added note about lost leading zeroes
    Example test string to decode: `9$B_,4-C$T(W_O;-)B'0N`
    Target decoding result: `0b0000101101000101100000100110100010111111100101001110110100111110110101001101010000001100010110100000001010011101101110100000011110`
    Actual decoding result in current version: `0b00101101000101100000100110100010111111100101001110110100111110110101001101010000001100010110100000001010011101101110100000011110`
    Current behavior: two leading zeroes missing from above string
    Solution to consider: add back leading zeroes when length of string output modulo 6 is greater than zero. 
    Is 22 characters and should decode to 132 bits but only decodes to 130 as two leading zeroes are lost and must be padded back.

--- base64Decode_v1_02.py
+++ base64Decode_v1_02.py
@@ -107,7 +107,7 @@ print(decodeAsBin(hatdata2decode))
 
 ## test decode string "HELLOWORLD" should return the binary equivalent of hex: 0x44e55562061b54d
 ## test decode string "INVENTEDBYSTEVENHATZAKIS@2018' should return this hex 0x125df39774e34b89c74e7ce5d129d8ca512739080048 as equivalent in binary to this output: 010010010111011111001110010111011101001110001101001011100010011100011101001110011111001110010111010001001010011101100011001010010100010010011100111001000010000000000001001000
-
+## Note/ WARNING: leading zeroes are not retained in the Hatnotated string, therefore, when decoding the test string `9$B_,4-C$T(W_O;-)B'0N` found in the Readme file, two leading zeroes will not appear. Software implementers should pad enough zeroes to math the Hatnotated character lenght (i.e. 22 pasted characters should compute 132 bits, but if only 130 bits show then 2 leading zeroes shoud be padded. 
 ## Note/CONSIDERATION: In version 1.01 to resolve a rendering conflict, the Forward slash `/` and Backslash `\` characters have been swapped out and replaced by the closing curly bracket `}` and opening curly bracket `{` on purpose.
 
 
